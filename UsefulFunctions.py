ef4da1c16a146f9fa721f7f68dc9545824517e34
Zeyn Schweyk <zeynschweyk@dpengineering.org>
2021-11-17 21:52:15 -0800

    Fixed round_to function to deal with edge cases, and work around python rounding issues.

--- UsefulFunctions.py
+++ UsefulFunctions.py
@@ -3,15 +3,21 @@ def round_to(num, nrst, limit):
     Rounds a number to the nearest tenth, quarter..., and makes sure to not exceed a given limit.
     """
     dec = num - int(num)
-    dec_remainder = dec % nrst
+    dec_remainder = round(dec % nrst, 5)
+
+    print(dec_remainder)
 
     if dec_remainder == 0:
         return num
     if dec_remainder >= nrst / 2:
-        result = int(num) + (dec - dec_remainder) + nrst
-        if result <= limit:
-            return result
-    return int(num) + (dec - dec_remainder)
+        rounded_up = int(num) + (dec - dec_remainder) + nrst
+        if rounded_up <= limit:
+            return round(rounded_up, 5)
+    rounded_down = int(num) + (dec - dec_remainder)
+    return round(rounded_down if rounded_down <= limit else limit, 5)
 
 
-print(round_to(80.46, .1, 80.5))
+print(round_to(80.149, .1, 80.5))
+
+# num = 8.5
+# print(f"{num} = {bin(num)}")
