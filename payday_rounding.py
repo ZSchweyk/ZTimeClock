6e33a14c04f8575b212af077ff78688aa920220f
ZSchweyk <zeynschweyk@dpengineering.org>
2022-04-07 21:12:08 -0700

    maybe this is working

--- payday_rounding.py
+++ payday_rounding.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 def round_to(num, nrst, limit):
     """
     Rounds a number to the nearest tenth, quarter..., and makes sure to not exceed a given limit.
@@ -18,6 +20,27 @@ def round_to(num, nrst, limit):
 
 
 def calculate_leave_time(clock_in: datetime, shift_end: datetime, max_day_hours, current_period_hours, max_period_hours):
+    max_day_hours = max(min(max_day_hours, max_period_hours - current_period_hours), 0)
     current_period_hours += max_day_hours
+    print(f"current_period_hours: {current_period_hours}")
+    rounded_total_period_hours_worked = round_to(current_period_hours, .25, 1000)
+    print(f"rounded_total_period_hours_worked: {rounded_total_period_hours_worked}")
+    difference = rounded_total_period_hours_worked - current_period_hours
+    max_day_hours += difference
+    temp_clock_out = clock_in + timedelta(hours=max_day_hours)
+    if temp_clock_out > shift_end:
+        max_day_hours -= (temp_clock_out - shift_end).seconds / 3600
+    final_clock_out = clock_in + timedelta(hours=max_day_hours)
+    return final_clock_out.strftime("%I:%M:%S %p")
 
+
+clock_in = datetime.strptime("6:00:00 am", "%I:%M:%S %p")
+shift_end = datetime.strptime("3:30:00 pm", "%I:%M:%S %p")
+max_day_hours = 8
+current_period_hours = 70.15
+max_period_hours = 80
+
+calculated_clock_out = calculate_leave_time(clock_in, shift_end, max_day_hours, current_period_hours, max_period_hours)
+
+print(f"Calculated Clock Out: {calculated_clock_out}")
 
